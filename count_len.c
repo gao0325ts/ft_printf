b371ba5bdae836bdc4108e815864a0d82e12ed21
stakada <stakada@student.42tokyo.jp>
2024-05-13 21:52:33 +0900

    update count_len.c

--- count_len.c
+++ count_len.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 20:35:44 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/13 21:03:22 by stakada          ###   ########.fr       */
+/*   Updated: 2024/05/13 21:52:21 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	count_padded_len(t_spec specs, int digits)
 {
-	int	p_len;
+	int	pd_len;
 
-	p_len = 0;
+	pd_len = 0;
 	if (specs.flags & FLAG_HASH)
-		p_len = 2;
+		pd_len = 2;
     if (num < 0 || specs.flags & FLAG_PLUS || specs.flags & FLAG_SPACE)
-		p_len++;
+		pd_len++;
 	if (digits < specs.precision)
-		p_len += specs.precision;
+		pd_len += specs.precision;
 	else
-		p_len += digits;
-	return (p_len);
+		pd_len += digits;
+	return (pd_len);
 }
